c10d5dfa81a42956b0ef8aa2ce7a21de7f2c515d
EdsonAguiar888 <edsonaguiar888@gmail.com>
2022-02-12 11:47:33 -0300

    Atualizacao e correcao de identacao dos arquivos index.html style.css e script.js.    Foi add 2 imagens referente ao projeto.

--- script.js
+++ script.js
@@ -4,11 +4,16 @@
 var btnSignin = document.querySelector("#sign-in");
 var btnSignup = document.querySelector("#sign-up");
 
+
 var body = document.querySelector("body")
+
+
 
 btnSignin.addEventListener("click", function () {
     body.className = "sign-in-js";
 })
+
+
 
 btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
